16b83933b283d1847e479403a736e64b0625ced1
Codeprince <anyaohaprince122@gmail.com>
2022-02-27 17:23:48 +0100

    incoorporated pylint change in tests

--- tests.py
+++ tests.py
@@ -1,14 +1,14 @@
 import unittest
-from translator import frenchToEnglish, englishToFrench
+from translator import french_to_english, english_to_french
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test1(self):
-        self.assertNotEqual(frenchToEnglish('null'), 'Hello')
-        self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
+        self.assertNotEqual(french_to_english('null'), 'Hello')
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
 
 class TestEnglishToFrench(unittest.TestCase):
     def test1(self):
-        self.assertNotEqual(englishToFrench('null'), 'Bonjour')
-        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
+        self.assertNotEqual(english_to_french('null'), 'Bonjour')
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
 
 unittest.main()
