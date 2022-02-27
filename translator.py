0bb4499409357fb2379cb9e478ab3e904fcf5a0b
Codeprince <anyaohaprince122@gmail.com>
2022-02-27 13:46:44 +0100

    added ibm cloud translatorinstance

--- translator.py
+++ translator.py
@@ -10,10 +10,11 @@ apikey = os.environ['API_KEY']
 url = os.environ['URL']
 
 
-authenticator = IAMAuthenticator('{apikey}')
+authenticator = IAMAuthenticator(apikey=apikey)
 language_translator = LanguageTranslatorV3(
     version='{version}',
     authenticator=authenticator
 )
 
 language_translator.set_service_url('{url}')
+
