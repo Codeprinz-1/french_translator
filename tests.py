182bc190e6054c742c9078eb8ddbf5e5a670fc94
Codeprince <anyaohaprince122@gmail.com>
2022-02-27 17:02:32 +0100

    retrieved translation from response

--- tests.py
+++ tests.py
@@ -3,12 +3,12 @@ from translator import frenchToEnglish, englishToFrench
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test1(self):
-        self.assertNotEqual(frenchToEnglish(), 'Hello')
+        self.assertNotEqual(frenchToEnglish('null'), 'Hello')
         self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
 
 class TestEnglishToFrench(unittest.TestCase):
     def test1(self):
-        self.assertNotEqual(englishToFrench(), 'Bonjour')
+        self.assertNotEqual(englishToFrench('null'), 'Bonjour')
         self.assertEqual(englishToFrench('Hello'), 'Bonjour')
 
 unittest.main()
