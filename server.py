1395e222ccbb36413c788052d972455eb21022b7
Codeprince <anyaohaprince122@gmail.com>
2022-02-27 12:44:40 +0100

    added english to french route

--- server.py
+++ server.py
@@ -1,3 +1,11 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
+
+app = Flask('Web Translator')
+
+@app.route('/englishToFrench')
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
+
+    return 'Translated text to French'
