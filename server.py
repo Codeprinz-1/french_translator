17896e445254f5e12012a41264f0d2b63d4bab86
Codeprince <anyaohaprince122@gmail.com>
2022-02-27 12:49:14 +0100

    defined french to english route

--- server.py
+++ server.py
@@ -9,3 +9,10 @@ def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
 
     return 'Translated text to French'
+
+@app.route('/frenchToEnglish')
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+
+    return 'Translated text to English'
+
