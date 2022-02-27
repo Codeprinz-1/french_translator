eb22d2cfc7d8f8983dc7f85b882e9463a77b3b7d
Codeprince <anyaohaprince122@gmail.com>
2022-02-27 13:00:32 +0100

    added translate to french function

---
+++ static/mywebscript.js
@@ -0,0 +1,16 @@
+let translateToFrench = () => {
+  textToTranslate = document.getElementById("textToTranslate").value;
+
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("translated_text").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open(
+    "GET",
+    "englishToFrench?textToTranslate" + "=" + textToTranslate,
+    true
+  );
+  xhttp.send();
+};
