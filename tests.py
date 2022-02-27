363fe82a58bedca43b8903d9ecf466ec5be8fe0a
Codeprince <anyaohaprince122@gmail.com>
2022-02-27 15:27:14 +0100

    defined test for english to french

--- tests.py
+++ tests.py
@@ -3,6 +3,10 @@ from unittest import TestCase
 
 class TestFrenchToEnglish(TestCase):
     def test1(self):
-        self.assertEqual(frenchToEnglish(), 'Hello')
-        self.assertEqual(frenchToEnglish('Hello'))
+        self.assertNotEqual(frenchToEnglish(), 'Hello')
+        self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
 
+class TestEnglishToFrench(TestCase):
+    def test1(self):
+        self.assertNotEqual(englishToFrench(), 'Bonjour')
+        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
